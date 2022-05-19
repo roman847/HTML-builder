e18de52908cb52bd087dbe2206d08798d055fd4d
Roman <romabogdan147@gmail.com>
2022-05-19 15:32:09 +0300

    feat: add task 04-copy-directory (Thu May 19 15:32:09 RTZ 2022)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+const fsPromises = fs.promises;
 
+async function createCopyDir() {
+  createCopy();
+  await removeFilesCopy();
+  readAndWriteCopyFiles();
+}
+createCopyDir();
+
+function createCopy() {
+  fsPromises
+    .mkdir(path.join(__dirname, "files-copy"), { recursive: true })
+    .catch(() => {
+      console.log("failed to create directory");
+    });
+}
+
+function removeFilesCopy() {
+  fs.readdir(path.join(__dirname, "files-copy"), (err, data) => {
+    if (err) throw err;
+    data.forEach((file) => {
+      fs.unlink(path.join(__dirname, "files-copy", `${file}`), (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+}
+
+function readAndWriteCopyFiles() {
+  const pathDir = path.join(__dirname, "files");
+
+  fs.readdir(pathDir, (err, data) => {
+    if (err) throw err;
+    data.forEach((file) => {
+      fs.writeFile(path.join(__dirname, "files-copy", `${file}`), "", (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+}
